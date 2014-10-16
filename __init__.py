c3201ba78e7d8b786be7bbb015ec67c638499b49
Mike Boyle <boyle@astro.cornell.edu>
2014-10-16 16:40:14 -0400

    Update init file for newer pythons

--- __init__.py
+++ __init__.py
@@ -1,12 +1,14 @@
-from info import __doc__
+import numpy as np
+
+from .numpy_quaternion import quaternion
+
+__doc_title__ = "Quaternion dtype for NumPy"
+__doc__ = "Adds a quaternion dtype to NumPy."
 
 __all__ = ['quaternion']
 
-import numpy
-from numpy_quaternion import quaternion
-
-if numpy.__dict__.get('quaternion') is not None:
+if np.__dict__.get('quaternion') is not None:
     raise RuntimeError('The NumPy package already has a quaternion type')
 
-numpy.quaternion = quaternion
-numpy.typeDict['quaternion'] = numpy.dtype(quaternion)
+np.quaternion = quaternion
+np.typeDict['quaternion'] = np.dtype(quaternion)
--- info.py
+++
@@ -1,2 +0,0 @@
-__doc_title__ = "Quaternion dtype for NumPy"
-__doc__ = "Adds a quaternion dtype to NumPy."
