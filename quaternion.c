6bb6e1920e6bddf0aacc46265f249607dc274c81
Mike Boyle <boyle@astro.cornell.edu>
2014-10-17 19:52:27 -0400

    Correct the division formula

--- quaternion.c
+++ quaternion.c
@@ -102,9 +102,9 @@ quaternion_divide(quaternion q1, quaternion q2)
   double s = q2.w*q2.w + q2.x*q2.x + q2.y*q2.y + q2.z*q2.z;
   return (quaternion) {
     (  q1.w*q2.w + q1.x*q2.x + q1.y*q2.y + q1.z*q2.z) / s,
-      (- q1.w*q2.x + q1.x*q2.w + q1.y*q2.z - q1.z*q2.y) / s,
-      (- q1.w*q2.y - q1.x*q2.z + q1.y*q2.w + q1.z*q2.x) / s,
-      (- q1.w*q2.z + q1.x*q2.y - q1.y*q2.x + q1.z*q2.w) / s
+      (- q1.w*q2.x + q1.x*q2.w - q1.y*q2.z + q1.z*q2.y) / s,
+      (- q1.w*q2.y + q1.x*q2.z + q1.y*q2.w - q1.z*q2.x) / s,
+      (- q1.w*q2.z - q1.x*q2.y + q1.y*q2.x + q1.z*q2.w) / s
       };
 }
 
