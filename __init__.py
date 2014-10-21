22d5d93c6842a7422adf8751f8ca62e2bd7498b0
Michael Boyle <michael.oliver.boyle@gmail.com>
2014-10-20 20:26:00 -0400

    Better check for clashing quaternion type; closes #6

--- __init__.py
+++ __init__.py
@@ -1,12 +1,5 @@
 import numpy as np
 
-# orig_import = __import__
-# def debug_import(name, globals=None, locals=None, fromlist=(), level=0):
-#     print("debug_import:", name, globals, locals, fromlist, level)
-#     return orig_import(name, globals, locals, fromlist, level)
-# import builtins
-# builtins.__import__ = debug_import
-
 from .numpy_quaternion import quaternion
 
 __doc_title__ = "Quaternion dtype for NumPy"
@@ -14,7 +7,7 @@ __doc__ = "Adds a quaternion dtype to NumPy."
 
 __all__ = ['quaternion']
 
-if np.__dict__.get('quaternion') is not None:
+if 'quaternion' in np.__dict__:
     raise RuntimeError('The NumPy package already has a quaternion type')
 
 np.quaternion = quaternion
