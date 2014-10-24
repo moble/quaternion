2c4c05a548d946ddac796b5c8126f064028942e5
Michael Boyle <michael.oliver.boyle@gmail.com>
2014-10-24 18:59:05 -0400

    Handle Travis issue with fast-math

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 def configuration(parent_package='',top_path=None):
     import numpy
+    import os
     from distutils.errors import DistutilsError
     if numpy.__dict__.get('quaternion') is not None:
         raise DistutilsError('The target NumPy already has a quaternion type')
     from numpy.distutils.misc_util import Configuration
     from numpy.distutils.system_info import get_info
+    if(os.environ.get('THIS_IS_TRAVIS') is not None):
+        print("This appears to be Travis!")
+        compile_args = ['-O3']
+    else:
+        compile_args = ['-ffast-math', '-O3']
     config = Configuration('quaternion',parent_package,top_path)
     config.add_extension('numpy_quaternion',
                          ['quaternion.c','numpy_quaternion.c'],
-                         extra_compile_args=['-O3', # Because some python builds use '-O1' or less!)
-                                             ],)
+                         extra_compile_args=compile_args,)
     return config
 
 if __name__ == "__main__":
