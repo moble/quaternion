3cc6b92a48a994a890a591c17fa185c8b573c589
Michael Boyle <michael.oliver.boyle@gmail.com>
2014-11-04 17:05:00 -0500

    File that doesn't do anything, though it might help numba in future [ci skip]
    This contains various things pulled from numba.types that could help
    to construct a Quaternions type within numba.  I've tried it without
    success, because evidently numba doesn't currently support these
    things.  Eventually, there should also be a registry of some sort, but
    not just yet.

---
+++ numba_quaternion.py
@@ -0,0 +1,35 @@
+from __future__ import print_function, division, absolute_import
+
+import itertools
+import numpy
+import weakref
+
+from numba import utils, float32, float64
+from numba.six import add_metaclass
+from numba.types import Type, number_domain
+
+@utils.total_ordering
+class Quaternion(Type):
+    def __init__(self, name, underlying_float, **kwargs):
+        super(Quaternion, self).__init__(name, **kwargs)
+        self.underlying_float = underlying_float
+        # Determine bitwidth
+        assert self.name.startswith('quaternion')
+        bitwidth = int(self.name[10:])
+        self.bitwidth = bitwidth
+
+    def cast_python_value(self, value):
+        return getattr(numpy, self.name)(value)
+
+    def __lt__(self, other):
+        if self.__class__ is not other.__class__:
+            return NotImplemented
+        return self.bitwidth < other.bitwidth
+
+quaternion128 = Quaternion('quaternion128', float32)
+quaternion256 = Quaternion('quaternion256', float64)
+quaternion_domain = frozenset([quaternion128, quaternion256])
+number_domain = number_domain | quaternion_domain
+
+q16 = quaternion128
+q32 = quaternion256
