33e4246fc66a6af92eb05905cd30ab60c9a47dbb
Michael Boyle <michael.oliver.boyle@gmail.com>
2014-12-11 18:19:26 -0500

    Add copyright notice and LICENSE references

--- quaternion_time_series.py
+++ quaternion_time_series.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2014, Michael Boyle
+# See LICENSE file for details: <https://github.com/moble/quaternion/blob/master/LICENSE>
+
 from __future__ import print_function, division, absolute_import
 
 import numpy as np
