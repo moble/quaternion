89c85f07f97e284c36dc31ea8b0b58f18df00c00
Michael Boyle <michael.oliver.boyle@gmail.com>
2015-02-16 13:15:26 -0500

    Make sure versions get output for each package; explain more

--- README.md
+++ README.md
@@ -7,19 +7,19 @@ by other python packages, and used by `setup.py` on installation.
 The `calculate_version` function constructs a version number from the date.
 This will be of the form
 
-    2015.02.04.82e4326
+    2015.02.15.82e4326
 
-where `2015.02.04` represents the date of the commit, and `82e4326` is the
+where `2015.02.15` represents the date of the commit, and `82e4326` is the
 shortened hash of the commit.  Additionally, if the code is not in a clean
 state (changes have been made since the last commit), then `.dirty` will be
 appended to the version.  This form requires the ability to run a few simple
-`git` commands from `python`.  If this is not possible, the system will fall
-back to the current date, and `installed_without_git` in place of the commit
-hash.
+`git` commands from `python`.  If that is not possible, the system will fall
+back to using the current date (at the time of installation), preceded by
+`0.0.0.`, so that version ordering will work safely.
 
 The `build_py_copy_version` class wraps the basic `build_py` class used in
 the standard setup function, but adds a step at the end to create a file named
-`version.py` that gets copied into the installed module.
+`_version.py` that gets copied into the installed module.
 
 To use both of these in the enclosing package, the enclosing `setup.py` file
 could contain something like this:
@@ -28,7 +28,7 @@ could contain something like this:
 import distutils.core
 from auto_version import calculate_version, build_py_copy_version
 
-distutils.core.setup(name='enclosing_package_name',
+distutils.core.setup(...,
                      version=calculate_version(),
                      cmdclass={'build_py': build_py_copy_version},
                      ...,)
@@ -37,7 +37,7 @@ distutils.core.setup(name='enclosing_package_name',
 And in the package's `__init__.py` file, you could have something like this:
 
 ```python
-from .version import __version__
+from ._version import __version__
 ```
 
 Then, in other code you would see the version normally:
--- __init__.py
+++ __init__.py
@@ -1,21 +1,31 @@
+"""Automatically create python package version info based on git date and hash
+
+This simple little module is intended to be included as a git submodule by
+other python packages, and used by setup.py on installation.  See the README at
+<https://github.com/moble/auto_version> for more details and instructions on
+how to use this module.
+
+"""
+
+
 def calculate_version():
     try:
         import subprocess
         git_revision = subprocess.check_output("git show -s --format='%cI %h' HEAD", shell=True)
         date, short_hash = git_revision.split(' ')
         date = date.split('T')[0]  # remove ISO 8601 time info
+        date = date.replace('-', '.')  # make date an acceptable version string
         short_hash = short_hash[:-1]  # remove newline
         dirty = bool(subprocess.call("git diff-files --quiet --", shell=True))
         dirty = dirty or bool(subprocess.call("git diff-index --cached --quiet HEAD --", shell=True))
+        vers = '{0}.{1}'.format(date, short_hash)
+        if dirty:
+            vers += '.dirty'
     except:
         from datetime import datetime
         date = datetime.now().isoformat().split('T')[0]
-        short_hash = 'installed_without_git'
-        dirty = False
-    date = date.replace('-', '.')
-    vers = '{0}.{1}'.format(date, short_hash)
-    if dirty:
-        vers += '.dirty'
+        date = date.replace('-', '.')
+        vers = '0.0.' + date
     return vers
 
 
@@ -28,5 +38,6 @@ class build_py_copy_version(build_py):
         build_py.run(self)  # distutils uses old-style classes, so no super()
         if not self.dry_run:
             import os.path
-            with open(os.path.join(self.build_lib, 'waveforms', 'version.py'), 'w') as fobj:
-                fobj.write('__version__ = "{0}"'.format(calculate_version()))
+            for package in self.packages:
+                with open(os.path.join(self.build_lib, os.path.join(package.split('.')), '_version.py'), 'w') as fobj:
+                    fobj.write('__version__ = "{0}"'.format(calculate_version()))
