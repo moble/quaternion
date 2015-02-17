fc1615a93a7fd9d5551d5ab302979ca6753b823d
Michael Boyle <michael.oliver.boyle@gmail.com>
2015-02-17 12:01:28 -0500

    Fix split bug, and remove useless print statement

--- __init__.py
+++ __init__.py
@@ -18,15 +18,15 @@ def calculate_version():
         short_hash = short_hash[:-1]  # remove newline
         dirty = bool(subprocess.call("git diff-files --quiet --", shell=True))
         dirty = dirty or bool(subprocess.call("git diff-index --cached --quiet HEAD --", shell=True))
-        vers = '{0}.{1}'.format(date, short_hash)
+        version = '{0}.{1}'.format(date, short_hash)
         if dirty:
-            vers += '.dirty'
+            version += '.dirty'
     except:
         from datetime import datetime
         date = datetime.now().isoformat().split('T')[0]
         date = date.replace('-', '.')
-        vers = '0.0.' + date
-    return vers
+        version = '0.0.0.' + date
+    return version
 
 
 from distutils.command.build_py import build_py
@@ -34,10 +34,9 @@ from distutils.command.build_py import build_py
 
 class build_py_copy_version(build_py):
     def run(self):
-        print(self.__dict__)
         build_py.run(self)  # distutils uses old-style classes, so no super()
         if not self.dry_run:
             import os.path
             for package in self.packages:
-                with open(os.path.join(self.build_lib, os.path.join(package.split('.')), '_version.py'), 'w') as fobj:
+                with open(os.path.join(self.build_lib, os.path.join(*package.split('.')), '_version.py'), 'w') as fobj:
                     fobj.write('__version__ = "{0}"'.format(calculate_version()))
