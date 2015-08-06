9ff81947fc08d2a51e4517b5ac4e13e22836db79
Michael Boyle <michael.oliver.boyle@gmail.com>
2015-08-06 18:56:53 -0400

    Add anaconda-client for Travis?

--- .travis_install.sh
+++ .travis_install.sh
@@ -14,13 +14,13 @@ if [[ "${CONDA}" == "true" ]]; then
   conda info -a
   conda config --set always_yes yes --set changeps1 no
   conda update -q conda
-  conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION numpy scipy numba pip pytest binstar
+  conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION numpy scipy numba pip pytest anaconda-client
   source activate test-environment
-#  conda config --add channels moble
-#  conda install quaternion
-#
-#else
-#
-#  python setup.py install
+  conda config --add channels moble
+  conda install quaternion
+
+else
+
+  python setup.py install
 
 fi
