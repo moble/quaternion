9764f1090f540678d5b5bf2b2971ab5d581c2e8e
Michael Boyle <michael.oliver.boyle@gmail.com>
2016-01-28 15:52:19 -0500

    Don't do conversions by default, but if you do, do a better job

--- update_anaconda_org.sh
+++ update_anaconda_org.sh
@@ -6,7 +6,8 @@ function usage {
     echo "Usage: update_anaconda_org.sh [conda_dir [conda_conversions [update_pypi]]]"
     echo
     echo '    conda_dir: Directory in which .binstar.yml is found, and any conversions are stored.  Defaults to `.`.'
-    echo '    conda_conversions: {osx-64,linux-32,linux-64,win-32,win-64,all}, passed to `conda convert`.  Defaults to `all`.'
+    echo '    conda_conversions: Comma-separated list of platforms to `conda convert` to.  Defaults to empty.'
+    echo '        Can take values from {osx-64,linux-32,linux-64,win-32,win-64,all}.'
     echo '    update_pypi: If given as `--pypi`, run `python setup.py register sdist upload`.  Defaults to `--no-pypi`.'
     exit 1
 }
@@ -17,7 +18,8 @@ fi
 
 CONDA_DIR=${1:-.}
 
-CONVERSIONS=${2:-all}
+CONVERSION=${2:-}
+IFS=',' read -ra CONVERSIONS <<< "$CONVERSION"
 
 PYPI=${3:---no-pypi}
 
@@ -29,10 +31,15 @@ do
     export CONDA_PY
     conda build --no-binstar-upload ${CONDA_DIR}
     mkdir -p ${CONDA_DIR}/conversions
-    conda convert -p ${CONVERSIONS} -o ${CONDA_DIR}/conversions `conda build ${CONDA_DIR} --output`
+    for conversion in "${CONVERSIONS[@]}"; do
+        conda convert -f -p ${conversion} -o ${CONDA_DIR}/conversions `conda build ${CONDA_DIR} --output`
+    done
 
 done
-conda server upload --force ${CONDA_DIR}/conversions/*/*tar.bz2
+conda server upload --force `conda build ${CONDA_DIR} --output`
+if [ ${#CONVERSIONS[@]} != 0 ]; then
+    conda server upload --force ${CONDA_DIR}/conversions/*/*tar.bz2
+fi
 
 if [ "$PYPI" == "--pypi" ]; then
     python setup.py register sdist upload
