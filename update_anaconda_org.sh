abaee680929d5cb0c9c913e667db1c36e8091d20
Michael Boyle <michael.oliver.boyle@gmail.com>
2016-01-28 15:53:34 -0500

    Squashed 'auto_version/' changes from f51980d..9764f10
    9764f10 Don't do conversions by default, but if you do, do a better job
    5a1ac39 Add help message and pypi option
    ff25acb Bump numpy version
    
    git-subtree-dir: auto_version
    git-subtree-split: 9764f1090f540678d5b5bf2b2971ab5d581c2e8e

--- update_anaconda_org.sh
+++ update_anaconda_org.sh
@@ -1,20 +1,46 @@
 #! /usr/bin/env sh
 set -e
 
-PWD=`pwd`
-DIR=`basename $PWD`
+function usage {
+    echo
+    echo "Usage: update_anaconda_org.sh [conda_dir [conda_conversions [update_pypi]]]"
+    echo
+    echo '    conda_dir: Directory in which .binstar.yml is found, and any conversions are stored.  Defaults to `.`.'
+    echo '    conda_conversions: Comma-separated list of platforms to `conda convert` to.  Defaults to empty.'
+    echo '        Can take values from {osx-64,linux-32,linux-64,win-32,win-64,all}.'
+    echo '    update_pypi: If given as `--pypi`, run `python setup.py register sdist upload`.  Defaults to `--no-pypi`.'
+    exit 1
+}
+if [[ $# == 1 && ( $1 == "-h" || $1 == "--help" ) ]]; then
+    usage;
+fi
 
-# anaconda-build trigger moble/${DIR}
 
-export CONDA_NPY=19
-CONDA_PYs=( 27 34 35 )
+CONDA_DIR=${1:-.}
 
+CONVERSION=${2:-}
+IFS=',' read -ra CONVERSIONS <<< "$CONVERSION"
+
+PYPI=${3:---no-pypi}
+
+CONDA_PYs=( 27 35 )
 for CONDA_PY in "${CONDA_PYs[@]}"
 do
 
-    echo CONDA_PY=${CONDA_PY} CONDA_NPY=${CONDA_NPY}
+    echo CONDA_PY=${CONDA_PY}
     export CONDA_PY
-    conda build --no-binstar-upload ${1:-.}
-    conda server upload --force `conda build ${1:-.} --output`
+    conda build --no-binstar-upload ${CONDA_DIR}
+    mkdir -p ${CONDA_DIR}/conversions
+    for conversion in "${CONVERSIONS[@]}"; do
+        conda convert -f -p ${conversion} -o ${CONDA_DIR}/conversions `conda build ${CONDA_DIR} --output`
+    done
 
 done
+conda server upload --force `conda build ${CONDA_DIR} --output`
+if [ ${#CONVERSIONS[@]} != 0 ]; then
+    conda server upload --force ${CONDA_DIR}/conversions/*/*tar.bz2
+fi
+
+if [ "$PYPI" == "--pypi" ]; then
+    python setup.py register sdist upload
+fi
