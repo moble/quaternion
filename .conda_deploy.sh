19222ee4d9c22969a7ddb9a6f84464a922408e58
Michael Boyle <michael.oliver.boyle@gmail.com>
2016-08-24 16:46:35 -0400

    Try installing conda-build and anaconda-client only in the root conda env

--- .conda_deploy.sh
+++ .conda_deploy.sh
@@ -5,6 +5,8 @@
 # >>> anaconda auth -c -n travis --max-age 307584000 --url https://anaconda.org/moble/quaternion --scopes "api:write api:read"
 # then go to the Travis-CI settings page for this package, and create an environment variable with that token.
 
+# I stole these ideas from Yoav Ram <https://gist.github.com/yoavram/05a3c04ddcf317a517d5>
+
 set -e
 
 if [[ "${CONDA}" != "true" ]]; then
@@ -12,6 +14,8 @@ if [[ "${CONDA}" != "true" ]]; then
 fi
 
 conda config --set anaconda_upload no;
+
+conda install -n root conda-build anaconda-client
 
 echo "Building package"
 conda build .
