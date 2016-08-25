85c8ddd4108acd85aed828983a1e61597b8ff62f
Michael Boyle <michael.oliver.boyle@gmail.com>
2016-08-25 13:51:23 -0400

    Make .meta_yaml_replacer.py work in python 2

--- .meta_yaml_replacer.py
+++ .meta_yaml_replacer.py
@@ -3,11 +3,14 @@
 # Copyright (c) 2016, Michael Boyle
 # See LICENSE file for details: <https://github.com/moble/quaternion/blob/master/LICENSE>
 
+from __future__ import print_function
+
 import fileinput
 from auto_version import calculate_version
 
 version_string = calculate_version()
 
-with fileinput.FileInput('meta.yaml', inplace=True) as file:
-    for line in file:
-        print(line.replace("version: '1.0'", "version: '{0}'".format(version_string)), end='')
+f = fileinput.FileInput('meta.yaml', inplace=True)
+for line in f:
+    print(line.replace("version: '1.0'", "version: '{0}'".format(version_string)), end='')
+f.close()
