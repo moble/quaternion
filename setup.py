600849f4854d72470e855c69d03c8ecaaf86c606
Michael Boyle <michael.oliver.boyle@gmail.com>
2016-08-25 11:53:40 -0400

    Set up pypi upload from Travis

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ def configuration(parent_package='', top_path=None):
 if __name__ == "__main__":
     from numpy.distutils.core import setup
 
-    setup(configuration=configuration,
+    setup(name='numpy-',
+          configuration=configuration,
           version=calculate_version(),
           cmdclass={'build_py': build_py_copy_version},)
