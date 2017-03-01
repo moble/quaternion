240672f782d4abb8d690a809ef516430a202d153
Michael Boyle <michael.oliver.boyle@gmail.com>
2017-03-01 14:14:51 -0500

    Remove py3.5 and add version string validation

--- setup.py
+++ setup.py
@@ -5,11 +5,9 @@
 
 from auto_version import calculate_version, build_py_copy_version
 
-
 def configuration(parent_package='', top_path=None):
     import numpy
     from distutils.errors import DistutilsError
-
     if numpy.__dict__.get('quaternion') is not None:
         raise DistutilsError('The target NumPy already has a quaternion type')
     from numpy.distutils.misc_util import Configuration
@@ -23,11 +21,18 @@ def configuration(parent_package='', top_path=None):
 
 
 if __name__ == "__main__":
+    from os import getenv
     from numpy.distutils.core import setup
+
+    validate = True
+    error_on_invalid = False
+    if getenv('CI') is not None:
+        if getenv('CI').lower() == 'true':
+            error_on_invalid = True
 
     setup(name='numpy-',
           configuration=configuration,
-          version=calculate_version(),
+          version=calculate_version(validate, error_on_invalid),
           cmdclass={'build_py': build_py_copy_version},
           url='https://github.com/moble/quaternion',
           author='Michael Boyle',
