0b1a85f4456da5199e539304c9dc3a7223d08485
Yin Li <eelregit@users.noreply.github.com>
2018-03-05 17:28:33 -0800

    Optimize sqrt (#69)

--- quaternion.c
+++ quaternion.c
@@ -41,12 +41,12 @@ quaternion
 quaternion_sqrt(quaternion q)
 {
   double absolute = quaternion_absolute(q);
-  if(fabs(1+q.w/absolute)<_QUATERNION_EPS) {
+  if(fabs(absolute+q.w)<_QUATERNION_EPS*absolute) {
     quaternion r = {0.0, sqrt(absolute), 0.0, 0.0};
     return r;
   } else {
-    double c = sqrt(absolute/(2+2*q.w/absolute));
-    quaternion r = {(1.0+q.w/absolute)*c, q.x*c/absolute, q.y*c/absolute, q.z*c/absolute};
+    double c = sqrt(0.5/(absolute+q.w));
+    quaternion r = {(absolute+q.w)*c, q.x*c, q.y*c, q.z*c};
     return r;
   }
 }
