13aef401404e21cadf9082f95572072b4b1e6759
Michael Boyle <michael.oliver.boyle@gmail.com>
2018-04-18 16:15:04 -0400

    Remove conda builds from deploy step

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,5 @@
 #! /bin/bash
 
-if [ -z "$ANACONDA_API_TOKEN" ]; then
-    echo "The variable 'ANACONDA_API_TOKEN' cannot be empty"
-    exit 1
-fi
-
-# if [[ -z $(git status -uno -s > /dev/null) ]]; then
-#     echo "All changes must be git committed"
-#     exit 1
-# fi
-
 export package_version=$(git log -1 --format=%cd --date=format:'%Y.%-m.%-d.%-H.%-M.%-S' || date +"%Y.%-m.%-d.%-H.%-M.%-S")
 echo "Building version '${package_version}'"
 
@@ -18,9 +8,6 @@ python setup.py sdist upload
 
 # Create all the osx binary pip packages
 ./deploy/build_macosx_wheels.sh "${package_version}"
-
-# Create all the osx conda packages
-conda build .
 
 # Start docker for the linux packages
 open --hide --background -a Docker
@@ -35,11 +22,3 @@ docker run -i -t \
     -v `pwd`:/code \
     -v `pwd`/deploy/build_manylinux_wheels.sh:/build_manylinux_wheels.sh \
     quay.io/pypa/manylinux1_x86_64 /build_manylinux_wheels.sh "${package_version}"
-
-# Create all the linux binary conda packages on centos 6
-docker run -i -t \
-    -e package_version \
-    -e ANACONDA_API_TOKEN \
-    -v ${HOME}/.condarc:/root/.condarc:ro \
-    -v `pwd`:/code \
-    moble/miniconda-centos bash -c 'conda build /code'
