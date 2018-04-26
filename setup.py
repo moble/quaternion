1403b0f321565a3189498beb9e6e822b6a1461e7
Michael Boyle <michael.oliver.boyle@gmail.com>
2018-04-26 09:35:02 -0400

    Allow for Windows to get a reasonable ersatz version number; closes #75

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (c) 2017, Michael Boyle
+# Copyright (c) 2018, Michael Boyle
 # See LICENSE file for details: <https://github.com/moble/quaternion/blob/master/LICENSE>
 
 # Construct the version number from the date and time this python version was created.
@@ -11,13 +11,26 @@ if "package_version" in environ:
 else:
     print("The variable 'package_version' was not present in the environment")
     try:
+        # For cases where this is being installed from git.  This gives the true version number.
+        from sys import platform
         from subprocess import check_output
+        on_windows = ('win' in platform.lower() and not 'darwin' in platform.lower())
+        use_shell = not on_windows
         version = check_output("""git log -1 --format=%cd --date=format:'%Y.%-m.%-d.%-H.%-M.%-S'""", shell=use_shell).decode('ascii').rstrip()
         print("Setup.py using git log version='{0}'".format(version))
     except:
-        from time import strftime, gmtime
-        version = strftime("%Y.%-m.%-d.%-H.%-M.%-S", gmtime())
-        print("Setup.py using strftime version='{0}'".format(version))
+        # For cases where this isn't being installed from git.  This gives the wrong version number,
+        # but at least it provides some information.
+        try:
+            from time import strftime, gmtime
+            try:
+                version = strftime("%Y.%-m.%-d.%-H.%-M.%-S", gmtime())
+            except ValueError:  # because Windows
+                version = strftime("%Y.%m.%d.%H.%M.%S", gmtime())
+            print("Setup.py using strftime version='{0}'".format(version))
+        except:
+            version = '0.0.0'
+            print("Setup.py failed to determine the version; using '{0}'".format(version))
 with open('_version.py', 'w') as f:
     f.write('__version__ = "{0}"'.format(version))
 
