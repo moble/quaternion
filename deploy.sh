3f5d5771478b119e39323f4bb1713a79e3323a20
Michael Boyle <michael.oliver.boyle@gmail.com>
2018-05-09 17:41:00 -0400

    Ensure that module can import after build and before deployment

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,10 @@ set -e
 export package_version=$(git log -1 --format=%cd --date=format:'%Y.%-m.%-d.%-H.%-M.%-S' || date +"%Y.%-m.%-d.%-H.%-M.%-S")
 echo "Building version '${package_version}'"
 
-# Rebuild and install locally, to ensure there are no warnings
+# Rebuild and install locally, then test trivial action, to ensure there are no warnings
 /bin/rm -rf build __pycache__
 CFLAGS='-Werror -Wall -Wextra' python setup.py install
+python -c 'import numpy as np; import quaternion; tmp = quaternion.quaternion(1,2,3,4)'
 
 # Create a pure source pip package
 python setup.py sdist upload
