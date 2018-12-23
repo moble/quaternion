2c782e4ed8146961264685077eec31c9a4d826a5
Michael Boyle <michael.oliver.boyle@gmail.com>
2018-12-24 11:08:55 +1300

    Make deployment script realistic

--- deploy.sh
+++ deploy.sh
@@ -12,22 +12,24 @@ CFLAGS='-Werror -Wall -Wextra' python setup.py install
 python -c 'import numpy as np; import quaternion; tmp = quaternion.quaternion(1,2,3,4)'
 
 # Create a pure source pip package
-python setup.py sdist upload
+/bin/rm -rf dist
+python setup.py sdist
+twine upload dist/*
 
 # Create all the osx binary pip packages
 ./deploy/build_macosx_wheels.sh "${package_version}"
 
-# Start docker for the linux packages
-open --hide --background -a Docker
-while ! (docker ps > /dev/null 2>&1); do
-    echo "Waiting for docker to start..."
-    sleep 1
-done
+# # Start docker for the linux packages
+# open --hide --background -a Docker
+# while ! (docker ps > /dev/null 2>&1); do
+#     echo "Waiting for docker to start..."
+#     sleep 1
+# done
 
-# Create all the linux binary pip packages on centos 5
-docker run -i -t \
-    -v ${HOME}/.pypirc:/root/.pypirc:ro \
-    -v `pwd`:/code \
-    -v `pwd`/deploy/build_manylinux_wheels.sh:/build_manylinux_wheels.sh \
-    -v `pwd`/deploy/pip_manylinux1_cache:/root/.cache/pip \
-    quay.io/pypa/manylinux1_x86_64 /build_manylinux_wheels.sh "${package_version}"
+# # Create all the linux binary pip packages on centos 5
+# docker run -i -t \
+#     -v ${HOME}/.pypirc:/root/.pypirc:ro \
+#     -v `pwd`:/code \
+#     -v `pwd`/deploy/build_manylinux_wheels.sh:/build_manylinux_wheels.sh \
+#     -v `pwd`/deploy/pip_manylinux1_cache:/root/.cache/pip \
+#     quay.io/pypa/manylinux1_x86_64 /build_manylinux_wheels.sh "${package_version}"
