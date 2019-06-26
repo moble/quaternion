986d57658c072cfcc4a7938496ec1100f3f46d66
Michael Boyle <michael.oliver.boyle@gmail.com>
2019-06-25 23:51:47 -0400

    Try to handle pip's failure to install numpy first

--- setup.py
+++ setup.py
@@ -28,11 +28,22 @@ else:
                 version = check_output("""git log -1 --format=%cd --date=format:'%Y.%-m.%-d.%-H.%-M.%-S'""", shell=True, stderr=devnull)
             version = version.decode('ascii').rstrip()
         print("Setup.py using git log version='{0}'".format(version))
-    except Exception:
+    except:
         pass
 if version is not None:
     with open('_version.py', 'w') as f:
         f.write('__version__ = "{0}"'.format(version))
+else:
+    try:
+        from ast import literal_eval
+        with open('_version.py', 'r') as f:
+            first_line = f.readline()
+        version_string = first_line.split('=')[1].strip()
+        version = literal_eval(version_string)
+    except:
+        pass
+
+
 
 
 long_description = """\
@@ -45,30 +56,68 @@ and Euler-angle representations of rotations.  The core of the code is written i
 
 
 if __name__ == "__main__":
-    import numpy
-    from setuptools import setup, Extension
-    # from distutils.core import setup, Extension
-    from distutils.errors import DistutilsError
-    if numpy.__dict__.get('quaternion') is not None:
-        raise DistutilsError('The target NumPy already has a quaternion type')
-    extension = Extension(
-        name='quaternion.numpy_quaternion',  # This is the name of the object file that will be compiled
-        sources=['quaternion.c', 'numpy_quaternion.c'],
-        extra_compile_args=['/O2' if on_windows else '-O3'],
-        depends=['quaternion.c', 'quaternion.h', 'numpy_quaternion.c'],
-        include_dirs=[numpy.get_include()]
+    # Note: Because pip may try to install this package before numpy (on which it depends, and which
+    # it needs to use *during* setup), we need to try to fail gracefully when numpy is not
+    # installed.  The following mostly follows the strategy found in scipy's setup.py script, here:
+    # https://github.com/scipy/scipy/blob/9ccc68475fc431c4a44c120693cf6878cc4c14a7/setup.py#L180
+    
+    import sys
+
+    setup_metadata = dict(
+        name='numpy-quaternion',  # Uploaded to pypi under this name
+        packages=['quaternion'],  # This is the actual package name
+        package_dir={'quaternion': ''},
+        install_requires=[
+            'numpy>=1.13',
+        ],
+        setup_requires=[
+            'numpy>=1.13',
+        ],
+        url='https://github.com/moble/quaternion',
+        author='Michael Boyle',
+        author_email='mob22@cornell.edu',
+        description='Add built-in support for quaternions to numpy',
+        long_description=long_description,
     )
-    setup(name='numpy-quaternion',  # Uploaded to pypi under this name
-          packages=['quaternion'],  # This is the actual package name
-          package_dir={'quaternion': ''},
-          ext_modules=[extension],
-          version=version,
-          install_requires=[
-              'numpy>=1.13',
-          ],
-          url='https://github.com/moble/quaternion',
-          author='Michael Boyle',
-          author_email='mob22@cornell.edu',
-          description='Add built-in support for quaternions to numpy',
-          long_description=long_description,
-    )
+
+    if version is not None:
+        setup_metadata['version'] = version
+
+    if len(sys.argv) >= 2 and ('--help' in sys.argv[1:] or
+            sys.argv[1] in ('--help-commands', 'egg_info', '--version',
+                            'clean')):
+        # For these actions, NumPy is not required.
+        #
+        # They are required to succeed without Numpy for example when
+        # pip is used to install Scipy when Numpy is not yet present in
+        # the system.
+        try:
+            from setuptools import setup
+        except ImportError:
+            from distutils.core import setup
+    else:
+        from setuptools import setup, Extension
+        from setuptools.command.build_ext import build_ext as _build_ext
+        import numpy
+        if numpy.__dict__.get('quaternion') is not None:
+            from distutils.errors import DistutilsError
+            raise DistutilsError('The target NumPy already has a quaternion type')
+        extension = Extension(
+            name='quaternion.numpy_quaternion',  # This is the name of the object file that will be compiled
+            sources=['quaternion.c', 'numpy_quaternion.c'],
+            extra_compile_args=['/O2' if on_windows else '-O3'],
+            depends=['quaternion.c', 'quaternion.h', 'numpy_quaternion.c'],
+            include_dirs=[numpy.get_include()]
+        )
+        setup_metadata['ext_modules'] = [extension]
+        # As suggested in https://stackoverflow.com/a/21621689/1194883:
+        class build_ext(_build_ext):
+            def finalize_options(self):
+                _build_ext.finalize_options(self)
+                # Prevent numpy from thinking it is still in its setup process:
+                __builtins__.__NUMPY_SETUP__ = False
+                import numpy
+                self.include_dirs.append(numpy.get_include())
+        setup_metadata['cmdclass'] = {'build_ext': build_ext}
+
+    setup(**setup_metadata)
