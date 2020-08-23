55fb4a593822e7c8d184cecae10cc27378853c5d
Michael Boyle <michael.oliver.boyle@gmail.com>
2020-08-23 01:48:33 -0400

    Ignore warnings

--- build.py
+++ build.py
@@ -3,24 +3,27 @@ from os.path import join
 from sys import platform
 import numpy as np
 
-if 'win' in platform.lower() and not 'darwin' in platform.lower():
-    extra_compile_args = ['/O2']
+if "win" in platform.lower() and not "darwin" in platform.lower():
+    extra_compile_args = ["/O2"]
 else:
-    extra_compile_args = ['-O3', '-w']
+    extra_compile_args = ["-O3", "-w"]
 
 extensions = [
     Extension(
-        name='quaternion.numpy_quaternion',  # This is the name of the object file that will be compiled
+        name="quaternion.numpy_quaternion",  # This is the name of the object file that will be compiled
         sources=[
-            'quaternion/quaternion.c',
-            'quaternion/numpy_quaternion.c'
+            "src/quaternion.c",
+            "src/numpy_quaternion.c"
         ],
         depends=[
-            'quaternion/quaternion.c',
-            'quaternion/quaternion.h',
-            'quaternion/numpy_quaternion.c'
+            "src/quaternion.c",
+            "src/quaternion.h",
+            "src/numpy_quaternion.c"
         ],
-        include_dirs=[np.get_include()],
+        include_dirs=[
+            np.get_include(),
+            "src"
+        ],
         extra_compile_args=extra_compile_args,
     )
 ]
