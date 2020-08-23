e30e1490cbfb64e62c898cabac01ae186da393b9
Michael Boyle <michael.oliver.boyle@gmail.com>
2020-08-23 00:36:25 -0400

    Add setup.py shim

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# This file is just a little shim to convince old tools to work.
+# See pyproject.toml and build.py for where the real work is done.
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
