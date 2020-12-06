dfda69792bc161f73a750e97eb804675b84b1fc5
Michael Boyle <michael.oliver.boyle@gmail.com>
2020-12-06 15:16:46 -0500

    Correct Actions naming of pip jobs

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import numpy as np
 
 
 # Set this first for easier replacement
-version = "2020.12.6.15.15.34"
+version = "2020.12.6.15.16.46"
 
 if "win" in platform.lower() and not "darwin" in platform.lower():
     extra_compile_args = ["/O2"]
