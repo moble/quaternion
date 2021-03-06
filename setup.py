42cf157773b68d24d5da7ce226c460b1b1bc92ec
Michael Boyle <michael.oliver.boyle@gmail.com>
2021-03-06 15:24:02 -0500

    Fit borders inside github workflow window [skip ci]

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import numpy as np
 
 
 # Set this first for easier replacement
-version = "2021.3.6.15.10.57"
+version = "2021.3.6.15.24.2"
 
 if "win" in platform.lower() and not "darwin" in platform.lower():
     extra_compile_args = ["/O2"]
--- src/quaternion/__init__.py
+++ src/quaternion/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2020, Michael Boyle
 # See LICENSE file for details: <https://github.com/moble/quaternion/blob/master/LICENSE>
 
-__version__ = "2021.3.6.15.10.57"
+__version__ = "2021.3.6.15.24.2"
 __doc_title__ = "Quaternion dtype for NumPy"
 __doc__ = "Adds a quaternion dtype to NumPy."
 __all__ = ['quaternion',
