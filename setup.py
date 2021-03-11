4601fb861ed386c4129f2975a5f7b3028c0eb341
Michael Boyle <michael.oliver.boyle@gmail.com>
2021-03-11 10:32:22 -0500

    Rename main branch

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import numpy as np
 
 
 # Set this first for easier replacement
-version = "2021.3.6.15.24.2"
+version = "2021.3.11.10.32.22"
 
 if "win" in platform.lower() and not "darwin" in platform.lower():
     extra_compile_args = ["/O2"]
