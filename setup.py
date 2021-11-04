866bf028d6fd81d8b143daa38753d62198fcd307
Michael Boyle <michael.oliver.boyle@gmail.com>
2021-11-04 13:07:40 -0400

    Specify ARCHS

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import numpy as np
 
 
 # Set this first for easier replacement
-version = "2021.11.4.13.2.28"
+version = "2021.11.4.13.7.40"
 
 if "win" in platform.lower() and not "darwin" in platform.lower():
     extra_compile_args = ["/O2"]
