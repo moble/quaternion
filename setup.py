dcebf6b7b9e195aa52e83ef91ad735fc613d0160
Michael Boyle <michael.oliver.boyle@gmail.com>
2021-11-04 13:11:50 -0400

    Correct spelling of `archs`

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import numpy as np
 
 
 # Set this first for easier replacement
-version = "2021.11.4.13.8.45"
+version = "2021.11.4.13.11.50"
 
 if "win" in platform.lower() and not "darwin" in platform.lower():
     extra_compile_args = ["/O2"]
