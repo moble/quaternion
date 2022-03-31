001065a044e9a8bb4274fef70bf3557ef2aa5921
Michael Boyle <michael.oliver.boyle@gmail.com>
2022-03-31 14:08:21 -0400

    Update version in pyproject.toml

--- .github/scripts/update_versions.py
+++ .github/scripts/update_versions.py
@@ -4,11 +4,13 @@ import fileinput
 
 
 def update(version):
+    files = ("pyproject.toml", "setup.py", "src/quaternion/__init__.py")
     pattern = re.compile('^(__version__|version) *= *".*?"')
     replacement = r'\1 = "' + version + '"'
-    with fileinput.input(files=("setup.py", "src/quaternion/__init__.py"), inplace=True) as f:
+    with fileinput.input(files=files, inplace=True) as f:
         for line in f:
             print(pattern.sub(replacement, line), end="")
+
 
 version = os.environ["new_version"]
 
