d72bd50136710989c83e0465be97cd036008581a
Michael Boyle <michael.oliver.boyle@gmail.com>
2022-03-31 10:44:26 -0400

    Avoid deprecation warning

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import numpy as np
 
 
 # Set this first for easier replacement
-version = "2022.2.10.14.20.39"
+version = "2022.3.31.10.44.26"
 
 if "win" in platform.lower() and not "darwin" in platform.lower():
     extra_compile_args = ["/O2"]
