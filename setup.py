fbaf57a5bd328a16c40885cbb87701a62f0d3308
Michael Boyle <michael.oliver.boyle@gmail.com>
2022-04-01 09:40:19 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import numpy as np
 
 
 # Set this first for easier replacement
-version = "2022.3.1"
+version = "2022.4.0"
 
 if "win" in platform.lower() and not "darwin" in platform.lower():
     extra_compile_args = ["/O2"]
