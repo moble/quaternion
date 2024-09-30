3f89d4b75a3b025b1108eba0ac03d393b7707ab7
Mike Boyle <michael.oliver.boyle@gmail.com>
2024-09-30 11:20:07 -0400

    Bring hatch version usage in line with `sxs` package

--- .github/scripts/parse_bump_rule.py
+++ .github/scripts/parse_bump_rule.py
@@ -1,18 +1,17 @@
-import sys
 import os
 
 
 def parse(message):
     if "#prerelease" in message:
-        return 'prerelease'
+        return "prerelease"
 
-    for pre in ['pre', '']:
-        for level in ['patch', 'minor', 'major']:
-            if f'#{pre}{level}' in message:
-                return f'{pre}{level}'
+    for pre in ["pre", ""]:
+        for level in ["patch", "minor", "major"]:
+            if f"#{pre}{level}" in message:
+                return f"{pre}{level}"
 
-    return 'patch'
+    return "patch"
 
 
-message = os.environ['github_event_head_commit_message']
+message = os.environ["github_event_head_commit_message"]
 print(parse(message))
